b384f4d9caa57eb3271bd0596624cd1421745e94
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-11-09 00:00:28 +0530

    little changes

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -14,7 +14,7 @@ export default function Register(){
         console.log(user)
         console.log("User Registered successfully")
     } catch (error) {
-        console.log(error.message   )
+        console.log(error.message)
     }
    }
    return(
